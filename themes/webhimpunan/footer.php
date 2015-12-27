3a94ae982320cbb5933a2b893b6213dd49d520ce
Ahmad N. F <fahmadnaufal@gmail.com>
2015-12-28 05:36:57 +0700

    Footer and Header edit

---
+++ themes/webhimpunan/footer.php
@@ -0,0 +1,67 @@
+
+     
+    <!-- ===========================
+    FOOTER START
+    =========================== -->    
+    <footer id="footer">
+        <div class="container">
+            <span class="bigicon icon-speedometer "></span>
+             
+            <div class="footerlinks"><!-- FOOTER LINKS START -->            
+                <ul>
+                    <li><a href="#hero">Home</a></li>
+                    <li><a href="#profile">profile</a></li>
+                    <li><a href="#achieve">achieve</a></li>
+                    <li><a href="#news">news</a></li>
+                    <li><a href="#gallery">gallery</a></li>
+
+                    <!--replace the email address below with your email address-->
+                    <li><a href="mailto:m@creatrix.us">Contact</a></li>                   
+                </ul>
+            </div><!-- FOOTER LINKS END -->
+             
+            <div class="footersocial wow fadeInUp" data-wow-duration="3s"><!-- FOOTER SOCIAL ICONS START -->
+                <ul>
+                    <li><a href="http://facebook.com/MamunSrizon"><span class="icon-social-facebook"></span></a></li>
+                    <li><a href="http://twitter.com/MamunSrizon"><span class="icon-social-twitter"></span></a></li>
+                    <li><a href="#"><span class="icon-social-youtube"></span></a></li>
+                    <li><a href="http://dribbble.com/srizon"><span class="icon-social-dribbble"></span></a></li>
+                    <li><a href="#"><span class="icon-social-tumblr"></span></a></li>
+                 </ul>
+             </div><!-- FOOTER SOCIAL ICONS END -->
+         </div>
+     </footer><!-- FOOTER END -->
+
+ 
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
+    <!--Other necessary scripts-->
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.jribbble-1.0.1.ugly.js"></script>
+    <script src='js/camera.js'></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/drifolio.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/hoverIntent.js"></script>
+    <script src="js/superfish.js"></script>
+    <script>new WOW().init();</script>
+
+    <!-- initialise Superfish -->
+    <script>
+
+    jQuery(document).ready(function(){
+      jQuery('ul.sf-menu').superfish();
+
+    });
+
+    </script>
+
+    <?php wp_footer(); ?>
+     
+  </body>
+</html>
--- themes/webhimpunan/header.php
+++ themes/webhimpunan/header.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">    
 
-    <!-- ===========================
-    SITE TITLE
-    =========================== -->
-    <title>Himpunan Mahasiswa Informatika ITB</title><!-- This is what you see on your browser tab-->
-    
-    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
-  
-   </head>
+        <!-- ===========================
+        SITE TITLE
+        =========================== -->
+        <title>Himpunan Mahasiswa Informatika ITB</title><!-- This is what you see on your browser tab-->
+        
+        <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+        <?php wp_head(); ?>
+
+    </head>
     <body data-spy="scroll">
